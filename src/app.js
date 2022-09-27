c558efcd15f68cb9673857c302080095118dba19
Renato Mendes <mendeslrenato@gmail.com>
2022-09-27 20:01:21 -0300

    add route /teams

--- src/app.js
+++ src/app.js
@@ -1,5 +1,22 @@
 const express = require('express');
 
+const teams = require('./data/teams');
+
 const app = express();
 
+app.get('/', (req, res) => res
+    .status(200)
+    .json(
+      {
+        message: 'Olá Mundo!',
+      },
+    ));
+
+app.get('/teams', (req, res) => res
+  .status(200)
+  .json(
+    {
+      teams,
+    },
+  ));    
 module.exports = app;
